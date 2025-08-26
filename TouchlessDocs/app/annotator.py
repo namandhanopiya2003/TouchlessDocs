d60fa0a478bdc0f71616712ec1ed535235093723
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-08-26 16:36:02 +0530

    Updated documentation

--- TouchlessDocs/app/annotator.py
+++ TouchlessDocs/app/annotator.py
@@ -1,6 +1,11 @@
 import cv2
 
+# Function to draw annotations (or dots) on a given frame
 def draw_annotation(frame, points):
+
+    # Loop through all points and draw a green circle for each
     for pt in points:
         cv2.circle(frame, pt, 5, (0, 255, 0), -1)
+
+    # Returns the frame with the drawn annotations
     return frame
