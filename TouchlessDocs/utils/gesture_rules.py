c913aa5438bee4948b47a3aa653a6e702721e063
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-08-30 15:33:49 +0530

    Updated gesture_rules.py

--- TouchlessDocs/utils/gesture_rules.py
+++ TouchlessDocs/utils/gesture_rules.py
@@ -1,18 +1,22 @@
 import math
 
+# Function to calculate distance between two points
 def distance(p1, p2):
     return math.hypot(p2[0] - p1[0], p2[1] - p1[1])
 
+# Checks if thumb and index finger are close (for pinch gesture)
 def is_pinch(hand_landmarks):
     thumb_tip = hand_landmarks[4]
     index_tip = hand_landmarks[8]
     return distance(thumb_tip, index_tip) < 0.04  
 
+# Checks if hand is swiping left
 def is_swipe_left(hand_landmarks):
     wrist = hand_landmarks[0]
     index_tip = hand_landmarks[8]
     return index_tip[0] < wrist[0] - 0.2 
 
+# Checks if all five fingers are open
 def is_five_fingers_open(hand_landmarks):
     fingers = [8, 12, 16, 20]
     return all(hand_landmarks[f][1] < hand_landmarks[f-2][1] for f in fingers)
